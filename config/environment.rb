9460eed406b62add13bd51bef27099a7466d5089
liormb <liormb@gmail.com>
2014-03-11 13:45:40 -0400

    setting up twitter api credentials in heroku and this application

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load the app's custom environment variables here, before environments/*.rb
+twitter_credentials = File.join(Rails.root, 'config', 'initializers', 'twitter_credentials.rb')
+load(twitter_credentials) if File.exists?(twitter_credentials)
+
 # Initialize the Rails application.
 Twendy::Application.initialize!
