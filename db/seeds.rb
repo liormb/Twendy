2c7d67d9c2a021f1744070256b2fe393a4df521b
liormb <liormb@gmail.com>
2014-03-11 14:52:49 -0400

    starting to build the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,18 @@
 # Creates countries and adds woeid
 # ================================
 
-# client = Twitter::REST::Client.new do |config|
-#   config.consumer_key        = "YOUR_CONSUMER_KEY"
-#   config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-#   config.access_token        = "YOUR_ACCESS_TOKEN"
-#   config.access_token_secret = "YOUR_ACCESS_SECRET"
-# end
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+end
+
+# get all country's names
+countries = []
+client.trends_available.each do |record|
+	countries << record[:attrs][:country]
+end
+
+countries.uniq!.sort!
+
