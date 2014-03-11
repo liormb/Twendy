2881c1d6e2dfe8737c8028e600df863e076e0f3b
liormb <liormb@gmail.com>
2014-03-11 12:00:06 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twendy::Application.config.secret_key_base = '11d24d2f4b7c9f46559e3345ec9ac981a183af34e816db7d9d0c50ce46f616be9c8890fb1e39e6db68791b977e458f3e0f16656da2c4141b7b8558a8106ed16b'
