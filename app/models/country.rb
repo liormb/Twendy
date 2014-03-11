ce3275252b0bfea208013e82255896576eb6faef
liormb <liormb@gmail.com>
2014-03-11 16:42:01 -0400

    finish the seed file: seeding the db with all available countries from Twitter with their woeid's

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,6 +2,10 @@ class Country < ActiveRecord::Base
 	has_many :countries_trends
 	has_many :trends, through: :countries_trends
 
-	validates_uniqueness_of :name
 	before_create :set_trends_time_updated_to_now
+	validates_uniqueness_of :name
+
+	def set_trends_time_updated_to_now
+		self.trends_updated = Time.now
+	end
 end
