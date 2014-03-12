d943884791eb5c557a368f13331e69d1cad922b5
liormb <liormb@gmail.com>
2014-03-11 23:57:36 -0400

    deleting the extra migration file

--- db/migrate/20140311161326_create_trends.rb
+++ db/migrate/20140311161326_create_trends.rb
@@ -2,7 +2,7 @@ class CreateTrends < ActiveRecord::Migration
   def change
     create_table :trends do |t|
       t.string :name
-      t.string :twitter_url
+      t.text :twitter_url
 
       t.timestamps
     end
--- db/migrate/20140312012651_change_trends_twitter_url_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTrendsTwitterUrlType < ActiveRecord::Migration
-  def change
-  	change_column :trends, :twitter_url, :text
-  end
-end
