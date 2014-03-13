47c1b31c5279303e45a154e942ad60ad745f4bd1
liormb <liormb@gmail.com>
2014-03-13 18:36:45 -0400

    making some more tweaks to the d3 heat map

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,26 +1,29 @@
 
 function heatMap(error, data) {
-	var d = data,
+	var 
     margin = { top: 50, right: 0, bottom: 100, left: 0 },
-    width = 880 - margin.left - margin.right,
-    height = 880 - margin.top - margin.bottom,
+    width = Math.floor(screen.width * 0.8) - margin.left - margin.right,
+    height = Math.floor((screen.height-100) * 0.8) - margin.top - margin.bottom,
     buckets = 10,
 		colors = ['#e56230','#dc7840','#d38b52','#c0a265','#a8b778','#89ca8c','#77c79f','#61c4b2','#45c1c4','#45a9c4','hsla(56,55%,85%,.8)'];
 		intervals = ["Now", "-2 hours", "-4 hr", "-6 hr", "-10 hr", "-12 hr", "-14 hr", "-16 hr", "-18 hr", "-20 hr", "-22 hr", "-24 hr"],
     trends = ["t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10"],
-    gridSize = Math.floor(width / intervals.length),
+    gridSize = 50,//Math.floor(width / intervals.length),
     legendElementWidth = gridSize;
 
+  //var d = data;
+
   var colorScale = d3.scale.quantile()
-      .domain([0, 6, d3.max(data, function (d) { return d.rank; })])
-      .range(colors);
+    .domain([0, 6, d3.max(data, function (d) { return d.rank; })])
+    .range(colors);
 
   // build the chart
-  var svg = d3.select(".chart-container").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select(".chart-container")
+  	.append("svg")
+    	.attr("width", width + margin.left + margin.right)
+    	.attr("height", height + margin.top + margin.bottom)
+    	.append("g")
+    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // build the trends
   var timeLabels = svg.selectAll(".trendLabel")
@@ -34,21 +37,21 @@ function heatMap(error, data) {
         .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "trendLabel mono axis axis-worktime" : "trendLabel mono axis"); });
 
   // build the interval
-  var dayLabels = svg.selectAll(".intervalLabel")
-      .data(intervals)
-      .enter().append("text")
-        .text(function(d) { return d; })
-        .attr("x", function(d, i) { return i * gridSize; })
-        .attr("y", 0)
-        .style("text-anchor", "middle")
-        .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-        .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "intervalLabel mono axis axis-workweek" : "intervalLabel mono axis"); });
+  // var dayLabels = svg.selectAll(".intervalLabel")
+  //     .data(intervals)
+  //     .enter().append("text")
+  //       .text(function(d) { return d; })
+  //       .attr("x", function(d, i) { return i * gridSize; })
+  //       .attr("y", 0)
+  //       .style("text-anchor", "middle")
+  //       .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+  //       .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "intervalLabel mono axis axis-workweek" : "intervalLabel mono axis"); });
 
   // combining everything
   var heatMap = svg.selectAll(".trend")
       .data(data)
       .enter().append("rect")
-      .attr("x", function(d) { return (d.interval - 1) * gridSize; })
+      .attr("x", function(d) { return (d.interval - 1) * (gridSize / 2); })
       .attr("y", function(d) { return (d.trend - 1) * gridSize; })
       .attr("rx", 4)
       .attr("ry", 4)
@@ -62,23 +65,23 @@ function heatMap(error, data) {
   heatMap.append("title").text(function(d) { return d.rank; });
    
   // building the color legend   
-  var legend = svg.selectAll(".legend")
-      .data([0].concat(colorScale.quantiles()), function(d) { return d; })
-      .enter().append("g")
-      .attr("class", "legend");
+  // var legend = svg.selectAll(".legend")
+  //     .data([0].concat(colorScale.quantiles()), function(d) { return d; })
+  //     .enter().append("g")
+  //     .attr("class", "legend");
 
-  legend.append("rect")
-    .attr("x", function(d, i) { return legendElementWidth * i; })
-    .attr("y", height)
-    .attr("width", legendElementWidth)
-    .attr("height", gridSize / 2)
-    .style("fill", function(d, i) { return colors[i]; });
+  // legend.append("rect")
+  //   .attr("x", function(d, i) { return legendElementWidth * i; })
+  //   .attr("y", height)
+  //   .attr("width", legendElementWidth)
+  //   .attr("height", gridSize / 2)
+  //   .style("fill", function(d, i) { return colors[i]; });
 
-  legend.append("text")
-    .attr("class", "mono")
-    .text(function(d, i) { return (i < 10) ? "rank " + Math.round(i+1) : "Not Ranked"; })
-    .attr("x", function(d, i) { return legendElementWidth * i; })
-    .attr("y", height + gridSize);
+  // legend.append("text")
+  //   .attr("class", "mono")
+  //   .text(function(d, i) { return (i < 10) ? "rank " + Math.round(i+1) : "Not Ranked"; })
+  //   .attr("x", function(d, i) { return legendElementWidth * i; })
+  //   .attr("y", height + gridSize);
 }
 
 function eventHandler() {
