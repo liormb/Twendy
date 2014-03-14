fa84182a172a8959a94b082597e1968ed8053410
liormb <liormb@gmail.com>
2014-03-14 00:26:56 -0400

    moving css roles to application.scss to solve pipline rails 4 issue with loading images from assets/images

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,14 +1,15 @@
 
 function heatMap(error, data) { 
-	var rectMargin = 3;
+	var rectMargin = 4;
   var margin = { top: 0, right: 0, bottom: 0, left: 0 };
   var width = Math.floor((screen.width-50) * 0.8) - margin.left - margin.right;
   var height = Math.floor((screen.height-150) * 0.8) - margin.top - margin.bottom;
   var buckets = 10;
-	var colors = ['rgba(40,40,40,.4)','rgb(139,0,64)','rgb(189,60,75)','rgb(220,110,59)','rgb(237,176,90)','rgb(248,226,134)','rgb(248,253,167)','rgb(132,195,165)','rgb(185,223,162)','rgb(81,134,191)','rgb(89,73,163)'];
+	//var colors = ['rgba(40,40,40,.4)','rgb(139,0,64)','rgb(189,60,75)','rgb(220,110,59)','rgb(237,176,90)','rgb(248,226,134)','rgb(248,253,167)','rgb(132,195,165)','rgb(185,223,162)','rgb(81,134,191)','rgb(89,73,163)'];
+	var colors = ['hsla(56,55%,85%,.5)','#e56230','#dc7840','#d38b52','#c0a265','#a8b778','#89ca8c','#77c79f','#61c4b2','#45c1c4','#45a9c4'];
 	var intervals = ["Now", "-2 hours", "-4 hr", "-6 hr", "-10 hr", "-12 hr", "-14 hr", "-16 hr", "-18 hr", "-20 hr", "-22 hr", "-24 hr"];
   var trends = ["t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10"];
-  var gridSize = 50;//Math.floor(width / intervals.length),
+  var gridSize = 54;//Math.floor(width / intervals.length),
   var legendElementWidth = gridSize;
 
   // building the color scale palete
@@ -52,8 +53,8 @@ function heatMap(error, data) {
      .append('rect')
       .attr('x', function(d){ return (d.interval / 2) * (gridSize + rectMargin) - gridSize - rectMargin; })
       .attr('y', function(d){ return d.trend * (gridSize + rectMargin) - gridSize - rectMargin; })
-      .attr('rx', 4)
-      .attr('ry', 4)
+      .attr('rx', 2)
+      .attr('ry', 2)
       .attr('class', 'interval bordered')
       .attr('width', gridSize)
       .attr('height', gridSize);
